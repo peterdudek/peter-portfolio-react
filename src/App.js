4b59e748ab3da389bfeed3b0ba45c7d8e68339a8
Piotr Dudek <piotr72@gmail.com>
2021-01-02 18:11:49 -0500

    updated file structure with react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
+import React from "react";
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Projects from "./pages/Projects";
+import Navbar from "./components/Navbar/navbar.js";
+import Footer from "./components/Footer/footer.js";
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
+      <Navbar />
+      <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/projects" component={Projects} />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Piotr Dudek - portfolio
         </p>
       </header>
+    <Footer />
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/Footer/footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Footer () {
+  return (
+    <div>Hi</div>
+  )
+}
+
+export default Footer; 
