1d9ff1bd2a34a507422ea7ec6985b3ee1fa2e6ac
Piotr Dudek <piotr72@gmail.com>
2021-02-06 17:12:56 -0500

    updated bio

--- src/pages/About.js
+++ src/pages/About.js
@@ -39,22 +39,21 @@ function About() {
 
         <h4>Pursuing the Coding Dream</h4>
         <p>
-          Full-Stack Web Developer leveraging project management and manufacturing compliance background to build and optimize web applications and create the ultimate user experience.
-          Currently enrolled in the Full-Stack Coding Bootcamp at Columbia University and working to obtain a certificate.
-          Strong technical skills in JavaScript, CSS, HTML, and responsive web design as well as a passion for learning new technologies and continual improvement.
+          Full-Stack Web Developer leveraging project management background to build web applications and create the ultimate user experience.
+          Recently graduated from the Full-Stack Coding Bootcamp at Columbia University.
+          Strong technical skills in JavaScript, CSS, HTML, and responsive web design as well as a passion for learning emerging technologies.
       </p>
         <br />
         <h4>Accomplishments</h4>
         <p>
-          Enhanced project management efficacy by implementing new PM software and revising operating procedures, which resulted in timely product launches and savings.
+          Enhanced company efficacy by implementing new PM software and revising operating procedures, which resulted in timely and cost-effective product launches.
           Successfully led the Quality Management Team and represented the company at the Annual ISO Inspections, improving results by 100% for two consecutive years.
           Awarded “Strength” at the ISO 9001 Audit for developing a cutting-edge Internal Audit Program.
       </p>
         <br />
         <h4>When I am not coding…</h4>
         <p>
-          Love travelling, animals, photography, spending time in nature, exploring exotic cuisines, and discovering new cultures and languages. Learned to speak Hebrew fluently.
-          Avid runner on the weekends.
+          Love travelling, animals, spending time in nature, and discovering new cultures and languages. Learned to speak Hebrew fluently.
           Favorite drink of choice: tequila with cranberry juice.
       </p>
       </div>
